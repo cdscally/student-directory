493762423f1548dd66ad8fef2fc9fbc5d40b1477
Colin Scally <colin.scally@gmail.com>
2017-04-25 22:08:15 +0100

    added array of arrays to hold cohort

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
 
 #let's put all students into an array
-students = ["Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates",
-"Bison the Puppy"]
+students = [
+  ["Dr. Hannibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Corleone", :november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator", :november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november],
+  ["Bison the Puppy", :november]
+  ]
 
 def print_header
   puts "The students of Villains Academy"
@@ -19,14 +21,14 @@ def print_header
 end
 
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall we have #{students.count} great students"
 end
 
 #nothing happens until we call the methods
